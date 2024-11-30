a21bfe29a6b098773357663fab869a4e7295db61
ALEXAIORUSHUN <fsccasebox@gmail.com>
2024-11-30 20:37:07 +0100

    RollerSkating
    Görkorcsolyázás

---
+++ RollerSkating/config.lua
@@ -0,0 +1,10 @@
+Config = {
+    MaxSpeedKmh = 50,
+	MaxSpeedSprintKmh = 45,
+    maxJumpHeigh = 5.0,
+    LoseConnectionDistance = 50.0,
+	RollerSlotHomme = 7,
+	RollerSlotFemme = 3,
+	RollerColorHomme = 2,
+	RollerColorFemme = 2,
+}
---
+++ RollerSkating/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'MenuV'
+version '1.0.0'
+description 'Rollerskating'
+author 'Patamods'
+
+client_scripts {
+    "config.lua",
+    'client.lua'
+}
